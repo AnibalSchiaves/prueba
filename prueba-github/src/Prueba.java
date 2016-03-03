6a7bb3fbffd1e04df4782b4fc4e28449fd9157cd
anibal <aschiav0@rosario.com.ar>
2016-03-03 11:57:14 -0300

    Corrección en metodo main

--- prueba-github/src/Prueba.java
+++ prueba-github/src/Prueba.java
@@ -1,7 +1,7 @@
 
 public class Prueba {
 	
-	public void main(String[] args) {
+	public static void main(String[] args) {
 		System.out.println("Este es un proyecto de java para probar con un repositorio en GitHub");
 	}
 
